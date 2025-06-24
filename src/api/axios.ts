f46ae812dc6003f343d5f3840e0fd8618779ab6e
shamselhawary123 <shamselhawary123@gmail.com>
2025-06-24 16:00:49 +0300

    fsu fsu

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,22 +1,26 @@
+// src/api/axios.ts
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  withCredentials: true,
+  timeout: 30000,
 });
 
-// Request interceptor for API calls
 api.interceptors.request.use(
   (config) => {
-    // console.log("Token being used:", localStorage.getItem("token"));
     const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
